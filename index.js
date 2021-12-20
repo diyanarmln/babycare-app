5705066a87bbf4d358800acd08ba41797637b30f
diyanarmln <diyana.rmln@gmail.com>
2021-12-20 22:45:24 +0800

    setup base code

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+// setup
+
+import express, { request, response } from 'express';
+import methodOverride from 'method-override';
+import cookieParser from 'cookie-parser';
+import pg from 'pg';
+
+// Initialise DB connection
+const { Pool } = pg;
+const pgConnectionConfigs = {
+  user: 'diyanaramlan',
+  host: 'localhost',
+  database: 'babycare',
+  port: 5432, // Postgres server always runs on this port by default
+};
+const pool = new Pool(pgConnectionConfigs);
+
+const app = express();
+app.use(cookieParser());
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+// Override POST requests with query param ?_method=PUT to be PUT requests
+app.use(methodOverride('_method'));
+// Configure Express to parse request body data into request.body
+app.use(express.urlencoded({ extended: false }));
